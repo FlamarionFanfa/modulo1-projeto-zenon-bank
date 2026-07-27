920567bab1b246afcf3a6db045e6b8da5092a830
FlamarionFanfa <flamarion.fanfa@gmail.com>
2026-07-27 00:39:01 -0300

    I18N

--- zenon-fraud-detector/src/main/java/ReportMain.java
+++ zenon-fraud-detector/src/main/java/ReportMain.java
@@ -2,24 +2,28 @@ import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.Currency;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class ReportMain {
     public static void main(String[] args) {
         TransactionReport report = new TransactionReport();
         TransactionReport.Statistics statistics = report.generateReport("data/PS_20174392719_1491204439457_log.csv");
 
-        var locale = Locale.of("pt", "BR");
+        //var locale = Locale.of("en", "US$");
+        var locale = Locale.of("en", "US");
         var integerFomatter = NumberFormat.getIntegerInstance(locale);
         var currencyFormatter =  DecimalFormat.getCurrencyInstance(locale);
-        currencyFormatter.setCurrency(Currency.getInstance("USD"));
+        //currencyFormatter.setCurrency(Currency.getInstance("USD"));
+
+        ResourceBundle bundle = ResourceBundle.getBundle("report", locale);
 
 
 
 
 
-        System.out.println("Total of lines: " + integerFomatter.format(statistics.totalTransactions()));
-        System.out.println("Total of frauds: " + integerFomatter.format(statistics.totalFrauds()));
-        System.out.println("Amount total frauds: " + currencyFormatter.format(statistics.totalAmount()));
+        System.out.println(bundle.getString("label.total.transctions")+ integerFomatter.format(statistics.totalTransactions()));
+        System.out.println(bundle.getString("label.total.frauds") + integerFomatter.format(statistics.totalFrauds()));
+        System.out.println(bundle.getString("label.total.amout")+ currencyFormatter.format(statistics.totalAmount()));
 
 
 
